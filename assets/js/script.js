64de0ee05c8e3fb464d4f825515a141100dc34fc
jackcrosbie <jack.crosbie@mycit.ie>
2021-08-10 16:28:01 +0000

    add basic html structure

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+let questions {
+    { 
+        prompt: "Vietnam claimed independence form which Country in 1945?\n(a) France\n\
+        (b) Spain\n(c) Netherlands\n (d)England",
+        answer: "a"
+    },
+    { 
+        prompt: "What family of fish does the anchovy belong to?\n(a) Herring\n\
+        (b) Cod\n(c) Mackerel\n (d)Grouper",
+        answer: "a"
+    },
+    { 
+        prompt: "In which country is the region making region the Yarra Valley?\n(a) Australia\n\
+        (b) South Africa\n(c) New Zealand\n (d)Portugal",
+        answer: "a"
+    },
+    { 
+        prompt: "How many countries in the world begin with the letter F?\n(a) 2\n\
+        (b) 3\n(c) 4\n (d)5",
+        answer: "b"
+    },
+    { 
+        prompt: "Kiwifruit is native to what country?\n(a) New Zealand\n\
+        (b) Australia\n(c) China\n (d)India",
+        answer: "c"
+    },
+
+    let score = 0;
