a3b494d5483d53840146e81c2c66be978f705fda
jackcrosbie <jack.crosbie@mycit.ie>
2021-08-10 23:33:54 +0000

    add basic javascript structure and altered html to suit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,69 @@
-let questions {
-    { 
-        prompt: "Vietnam claimed independence form which Country in 1945?\n(a) France\n\
-        (b) Spain\n(c) Netherlands\n (d)England",
-        answer: "a"
-    },
-    { 
-        prompt: "What family of fish does the anchovy belong to?\n(a) Herring\n\
-        (b) Cod\n(c) Mackerel\n (d)Grouper",
-        answer: "a"
-    },
-    { 
-        prompt: "In which country is the region making region the Yarra Valley?\n(a) Australia\n\
-        (b) South Africa\n(c) New Zealand\n (d)Portugal",
-        answer: "a"
-    },
-    { 
-        prompt: "How many countries in the world begin with the letter F?\n(a) 2\n\
-        (b) 3\n(c) 4\n (d)5",
-        answer: "b"
-    },
-    { 
-        prompt: "Kiwifruit is native to what country?\n(a) New Zealand\n\
-        (b) Australia\n(c) China\n (d)India",
-        answer: "c"
-    },
+// grab button element
+let button = document.querySelector('.start-btn');
+let quizArea = document.querySelector('.quiz-area');
+let shuffledQuestions, currentQuestionIndex;
+let questionElement = document.getElementById('question');
+let answerButtonElement = document.getElementById('answer-buttons');
 
-    let score = 0;
+
+button.onclick = function(startGame) {
+    button.classList.add('hide');
+    quizArea.classList.remove('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    setNextQuestion();
+
+}
+
+function setNextQuestion() {
+    
+  }
+  
+  function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+      let button = document.createElement('button');
+      button.innerText = answer.text;
+      button.classList.add('btn');
+      if (answer.correct) {
+        button.dataset.correct = answer.correct;
+      }
+      button.addEventListener('click', selectAnswer);
+      answerButtonsElement.appendChild(button);
+    })
+  }
+
+  let questions = [
+    {
+      question: 'What is 2 + 2?',
+      answers: [
+        { text: '4', correct: true },
+        { text: '22', correct: false }
+      ]
+    },
+    {
+      question: 'Who is the best YouTuber?',
+      answers: [
+        { text: 'Web Dev Simplified', correct: true },
+        { text: 'Traversy Media', correct: true },
+        { text: 'Dev Ed', correct: true },
+        { text: 'Fun Fun Function', correct: true }
+      ]
+    },
+    {
+      question: 'Is web development fun?',
+      answers: [
+        { text: 'Kinda', correct: false },
+        { text: 'YES!!!', correct: true },
+        { text: 'Um no', correct: false },
+        { text: 'IDK', correct: false }
+      ]
+    },
+    {
+      question: 'What is 4 * 2?',
+      answers: [
+        { text: '6', correct: false },
+        { text: '8', correct: true }
+      ]
+    }
+  ]
